f68ecce04cad2dffa32a1cd31e3fe3e8fb346261
shge <25113178+shge@users.noreply.github.com>
2019-03-25 21:08:27 +0900

    Implement timer

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 $(function() {
 
+setInterval(function(){console.log(moment().format('HH:mm:ss'));},30000); // dev
+setInterval(function(){ // Prevent sleep
+  setTimeout(function() {
+    document.getElementById('video').play();
+    console.log('Video played');
+  }, 100);
+}, 30000);
 
+var target_time;
+$('#start').on('click', function() {
+  target_time = moment().add(moment.duration('0:25:00'));
+  $('#timer').text('25:00'); // temp
+});
+
+var beep = new Audio('beep_short.mp3');
+setInterval(function(){
+  var diff = moment.duration(
+    target_time.diff(moment())
+  ).format('mm:ss', { trim: false });
+  console.log(diff);
+  if (diff.match(/00:00/)) {
+    diff = '00:00';
+    beep.play();
+    console.log('Played: ' + moment().format());
+  } else if (diff.match(/^-/)) {
+    diff = '00:00';
+  }
+  $('#timer').text(diff);
+}, 1000);
+
+var timer = $('#timer').text();
+moment.duration(timer);
 
 
 }); // $
